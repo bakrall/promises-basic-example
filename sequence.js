84e6c1e86e3829d7f05f1d1fdf4b769368ea5bf2
Barbara Stadnik <barbara1stadnik@gmail.com>
2020-05-15 08:17:33 +0200

    add comment

--- sequence.js
+++ sequence.js
@@ -1,3 +1,5 @@
+//previously, each variable was a promise, now each is a function returning a promise
+
 const cleanRoom = () => new Promise((resolve, reject) => resolve('room is cleaned'));
 const removeGarbage = (message) => new Promise((resolve, reject) => resolve(message + ' garbage is removed'));
 const winIcecream = (message) => new Promise((resolve, reject) => resolve(message + ' ice cream is won'));
