123abba20bf44cadb3ecb0547f732a17121f36a5
Barbara Stadnik <barbara1stadnik@gmail.com>
2020-05-15 08:24:15 +0200

    get result once all promises are done in parallel

---
+++ parallel.js
@@ -0,0 +1,5 @@
+const cleanRoom = new Promise((resolve, reject) => resolve('room is cleaned'));
+const removeGarbage = new Promise((resolve, reject) => resolve('garbage is removed'));
+const winIcecream = new Promise((resolve, reject) => resolve('ice cream is won'));
+
+Promise.all([cleanRoom, removeGarbage, winIcecream]).then(console.log('all finished')); 
