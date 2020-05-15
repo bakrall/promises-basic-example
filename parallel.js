f64a770195ead4cbc12a9f8471cccb0f71023934
Barbara Stadnik <barbara1stadnik@gmail.com>
2020-05-15 08:27:49 +0200

    refactor - with functions returning promises

--- parallel.js
+++ parallel.js
@@ -1,5 +1,5 @@
-const cleanRoom = new Promise((resolve, reject) => resolve('room is cleaned'));
-const removeGarbage = new Promise((resolve, reject) => resolve('garbage is removed'));
-const winIcecream = new Promise((resolve, reject) => resolve('ice cream is won'));
+const cleanRoom = () => new Promise((resolve, reject) => resolve('room is cleaned'));
+const removeGarbage = () => new Promise((resolve, reject) => resolve('garbage is removed'));
+const winIcecream = () => new Promise((resolve, reject) => resolve('ice cream is won'));
 
-Promise.all([cleanRoom, removeGarbage, winIcecream]).then(console.log('all finished')); 
+Promise.all([cleanRoom(), removeGarbage(), winIcecream()]).then(() => console.log('all finished'));
